4b3b5a8d4b6f1e0557373875dff4aab4b7abfe6a
martekbarek <bartek.marek40@gmail.com>
2020-12-10 17:34:05 +0100

    gicik

--- lista/src/main/java/com/paka/Controller/Controller.java
+++ lista/src/main/java/com/paka/Controller/Controller.java
@@ -6,13 +6,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
-import org.springframework.ui.Model;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -194,45 +192,7 @@ public class Controller {
 		return mv;
 	}
 	
-	/*
-	@RequestMapping("edit")
-public ModelAndView edit(@RequestParam("id") int id,@RequestParam("content") String content,@RequestParam("data") int data,HttpServletRequest request,HttpServletResponse response) {
-		
-		ModelAndView mv = new ModelAndView();
-		
-		SessionFactory factory = new Configuration()
-				.configure("hibernate.cfg.xml")
-				.addAnnotatedClass(Task.class)
-				.buildSessionFactory();
-		
-		Session session = factory.getCurrentSession();
-		
-		try {
-			session.beginTransaction();
-			
-		
-			session.createQuery("update Task set content= '"+content+"' where id=" + id).executeUpdate();
-			session.createQuery("update Task set data= '"+data+"' where id=" + id).executeUpdate();
-
-			List<Task> tasks= session.createQuery("from Task").getResultList();
-			mv.addObject("tasks", tasks);
-			session.getTransaction().commit();
-			
-		} finally {
-			
-			
-			factory.close();
-			
-		}
-		
-		
-		mv.setViewName("allTasks");
-	   
-		
-		return mv;
-	}
 	
-	*/
 	@RequestMapping("/edit")
 	public ModelAndView edit(
 			@Valid @ModelAttribute("task") Task theTask, BindingResult theBindingResult) {
--- lista/src/main/webapp/editTask.jsp
+++ lista/src/main/webapp/editTask.jsp
@@ -15,7 +15,7 @@
 	<form:form method="post" action="edit" modelAttribute="task">
 	
 		<form:hidden path="id"/>
-		Content:  <form:input path="content"/> <form:errors path="content" cssClass="error"/>
+		Content:  <form:input path="content"/> <form:errors style="color:red" path="content" cssClass="error"/>
 		
 		Deadline: <form:input path="data"/><br>
 		
